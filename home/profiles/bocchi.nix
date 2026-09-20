6c0d16a2b84a970977cbf769804459e2fb31d746
didodazai <73555711+didodazai@users.noreply.github.com>
2026-09-20 00:11:19 -0300

    home: add bocchi.nix

---
+++ home/profiles/bocchi.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  imports = [
+    ../desktop/mango.nix
+    ../desktop/noctalia.nix
+    ../programs/packages.nix
+    ../programs/zen.nix
+    ../shell/fish.nix
+    ../terminal/foot.nix
+    ../theme.nix
+  ];
+}
