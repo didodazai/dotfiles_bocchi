d9d98328cdc57819c3a141b3fb2e89197fe7694e
didodazai <73555711+didodazai@users.noreply.github.com>
2026-09-20 01:13:25 -0300

    waydir: package official AppImage with native core

--- home/programs/waydir.nix
+++ home/programs/waydir.nix
@@ -1,8 +1,34 @@
-{ inputs, pkgs, ... }:
+{ pkgs, ... }:
 
+let
+  version = "0.23.0";
+
+  waydir = pkgs.appimageTools.wrapType2 {
+    pname = "waydir";
+    inherit version;
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/Waydir/Waydir/releases/download/v${version}/waydir-${version}%2B30-linux.AppImage";
+      hash = "sha256-BOTUrPzB9ar0gRTmkxbz4ZCxgOJ2J0BPwDTo9pkC4BQ=";
+    };
+  };
+
+  waydirDesktop = pkgs.makeDesktopItem {
+    name = "waydir";
+    desktopName = "Waydir";
+    genericName = "File Manager";
+    comment = "Fast, keyboard-first desktop file manager";
+    exec = "waydir %U";
+    icon = "system-file-manager";
+    categories = [ "System" "FileTools" "FileManager" ];
+    mimeTypes = [ "inode/directory" ];
+    startupNotify = true;
+  };
+in
 {
   home.packages = [
-    inputs.waydir.packages.${pkgs.system}.default
+    waydir
+    waydirDesktop
   ];
 
   xdg.mimeApps = {
