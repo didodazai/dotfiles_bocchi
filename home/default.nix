94ad5fa7cbd4dffedfa12b925a8b76b38977758d
didodazai <73555711+didodazai@users.noreply.github.com>
2026-09-20 00:13:19 -0300

    home: split configuration into modules

--- home/default.nix
+++ home/default.nix
@@ -4,26 +4,12 @@
   imports = [
     inputs.noctalia.homeModules.default
     inputs.zen-browser.homeModules.beta
+    ./profiles/bocchi.nix
   ];
 
   home.username = username;
   home.homeDirectory = "/home/${username}";
-  home.stateVersion = "26.05";   # NÃO mudar em upgrades
+  home.stateVersion = "26.05"; # NÃO mudar em upgrades
 
   programs.home-manager.enable = true;
-
-  programs.fish = {
-    enable = true;
-    shellAbbrs = {
-      osaragi = "sudo nixos-rebuild switch --flake ~/nixos-config#bocchi";
-    };
-  };
-  programs.alacritty.enable = true;
-
-  # Noctalia sem "settings" por enquanto: configure pela GUI primeiro,
-  # depois migramos para cá o que valer a pena fixar.
-  programs.noctalia.enable = true;
-  programs.zen-browser.enable = true;
-
-  xdg.configFile."mango/config.conf".source = ./mango.conf;
 }
