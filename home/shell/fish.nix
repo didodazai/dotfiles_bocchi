1f9f7c2a6525d75f88f971e265af76ad5b5a9e1e
didodazai <73555711+didodazai@users.noreply.github.com>
2026-09-20 00:12:11 -0300

    home: add fish.nix

---
+++ home/shell/fish.nix
@@ -0,0 +1,42 @@
+{ config, ... }:
+
+{
+  home.sessionVariables.STARSHIP_CACHE = "${config.xdg.cacheHome}/starship";
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set -g fish_greeting
+    '';
+    shellAbbrs = {
+      osaragi = "sudo nixos-rebuild switch --flake ~/nixos-config#bocchi";
+      osaragi-test = "sudo nixos-rebuild test --flake ~/nixos-config#bocchi";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      character = {
+        success_symbol = "[›](bold green)";
+        error_symbol = "[›](bold red)";
+      };
+      git_status = {
+        deleted = "✗";
+        modified = "✶";
+        staged = "✓";
+        stashed = "≡";
+      };
+      nix_shell = {
+        symbol = " ";
+        heuristic = true;
+      };
+    };
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+}
